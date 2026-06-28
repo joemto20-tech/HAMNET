fd83abdb0455570060db0a62ba20710bcb5e35df
joemto20-tech <joemto20@gmail.com>
2026-06-28 06:33:20 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,11 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("hamAPI", {
-    getRadd: () => ipcRenderer.invoke("get-radd")
+    getRadd: () => ipcRenderer.invoke("get-radd"),
+    getSession: () => ipcRenderer.invoke("get-session"),
+    openNvmpLogin: () => ipcRenderer.invoke("open-nvmp-login"),
+    getCharacter: () => ipcRenderer.invoke("get-character"),
+    getGroups: () => ipcRenderer.invoke("get-groups"),
+    getGroupList: () => ipcRenderer.invoke("get-grouplist"),
+    getGroupDetail: (id) => ipcRenderer.invoke("get-group-detail", id)
 });
